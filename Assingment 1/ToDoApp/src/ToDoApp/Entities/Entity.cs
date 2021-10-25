c7bc9311049b8b5b0fd8ecc4d8e5646fa71443e2
nyazadzhiev <nyazadzhiev@gmail.com>
2021-10-25 18:35:05 +0300

    Upload codebase developed on personal repo

---
+++ Assingment 1/ToDoApp/src/ToDoApp/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Assingment 1/ToDoApp/src/ToDoApp/Entities/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Entities
+{
+    class Task : Entity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public User Creator { get; set; }
+        public TaskList ToDoList { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id {this.Id} \n" +
+                $"Title: {this.Title} \n" +
+                $"Description: {this.Description} \n" +
+                $"Is completed: {this.IsComplete} \n" +
+                $"User: {this.Creator.FirstName} {this.Creator.LastName}";
+        }
+    }
+}
