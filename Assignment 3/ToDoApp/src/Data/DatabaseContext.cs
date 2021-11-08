120d904a6656a82133172fa71386b8a0a319a9e3
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-08 20:34:16 +0200

    Created models

--- Assignment 3/ToDoApp/src/Data/DatabaseContext.cs
+++ Assignment 3/ToDoApp/src/Data/DatabaseContext.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 using System.Text.Json.Serialization;
 using System.Text.Json;
 using System.Data.SqlClient;
-using System.Data;
+using System.Data.Entity;
 using ToDoAppEntities;
 using Task = ToDoAppEntities.Task;
 using Microsoft.EntityFrameworkCore;
-using System.Data.Entity;
 
 namespace ToDoAppData
 {
@@ -28,16 +27,40 @@ namespace ToDoAppData
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            SetupUserConfiguration(modelBuilder);
+            SetupTaskListConfiguration(modelBuilder);
+            SetupTaskConfiguration(modelBuilder);
 
-            modelBuilder.Entity<User>().HasRequired(u => u.Creator).WithMany();
-            modelBuilder.Entity<User>().HasRequired(u => u.Modifier).WithMany();
-            modelBuilder.Entity<TaskList>().HasRequired(t => t.Creator).WithMany();
-            modelBuilder.Entity<TaskList>().HasRequired(t => t.Modifier).WithMany();
-            modelBuilder.Entity<Task>().HasRequired(t => t.ToDoList).WithMany(l => l.Tasks);
-            modelBuilder.Entity<Task>().HasRequired(t => t.Creator).WithMany();
-            modelBuilder.Entity<Task>().HasRequired(t => t.Modifier).WithMany();
+            base.OnModelCreating(modelBuilder);
         }
 
+        private static void SetupTaskConfiguration(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().HasKey(t => t.Id);
+            modelBuilder.Entity<Task>().Property(t => t.Title).IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<Task>().Property(t => t.Description).IsRequired().HasMaxLength(300);
+            modelBuilder.Entity<Task>().HasRequired(t => t.ToDoList).WithMany(l => l.Tasks).HasForeignKey(t => t.ListId).WillCascadeOnDelete(false);
+            modelBuilder.Entity<Task>().HasRequired(t => t.Creator).WithMany().HasForeignKey(u => u.CreatorId).WillCascadeOnDelete(false);
+            modelBuilder.Entity<Task>().HasRequired(t => t.Modifier).WithMany().HasForeignKey(u => u.ModifierId).WillCascadeOnDelete(false);
+        }
+
+        private static void SetupTaskListConfiguration(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskList>().HasKey(l => l.Id);
+            modelBuilder.Entity<TaskList>().Property(l => l.Title).IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<TaskList>().HasRequired(l => l.Creator).WithMany().HasForeignKey(u => u.CreatorId).WillCascadeOnDelete(false);
+            modelBuilder.Entity<TaskList>().HasRequired(l => l.Modifier).WithMany().HasForeignKey(u => u.ModifierId).WillCascadeOnDelete(false);
+        }
+
+        private static void SetupUserConfiguration(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Username).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().HasOptional(u => u.Creator).WithMany().HasForeignKey(u => u.CreatorId).WillCascadeOnDelete(false);
+            modelBuilder.Entity<User>().HasOptional(u => u.Modifier).WithMany().HasForeignKey(u => u.ModifierId).WillCascadeOnDelete(false);
+        }
     }
 }
