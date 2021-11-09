357a0d8892d5159988eb9e68afd20013f4606ec3
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-09 19:38:26 +0200

    Created Entities for sharedLists and assignedTasks

---
+++ Assignment 3/ToDoApp/src/Entitites/SharedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppEntities
+{
+    public class SharedList
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ListId { get; set; }
+        public User User { get; set; }
+        public TaskList List { get; set; }
+    }
+}
