0e4440459320eea24b52bb42bf08baa760e1a134
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-18 11:51:44 +0200

    Added List of Projects

--- Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Entities/Team.cs
+++ Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Entities/Team.cs
@@ -7,10 +7,12 @@ namespace ProjectManagementApp.DAL.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public List<User> Users { get; set; }
+        public List<Project> Projects { get; set; }
 
         public Team()
         {
             this.Users = new List<User>();
+            this.Projects = new List<Project>();
         }
     }
 }
