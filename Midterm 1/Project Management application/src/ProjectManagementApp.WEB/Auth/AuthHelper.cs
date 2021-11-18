5285173b65b9363b82582bcd8c215fb6139fd805
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-18 22:14:38 +0200

    Implemented user controller

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.WEB/Auth/AuthHelper.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using ProjectManagementApp.BLL.Services;
+using ProjectManagementApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.WEB.Auth
+{
+    public static class AuthHelper
+    {
+        public static async Task<User> GetCurrentUser(this UserService userService, HttpRequest request)
+        {
+            StringValues UsernameAuthHeader;
+            StringValues PasswordAuthHeader;
+
+            request.Headers.TryGetValue("Username", out UsernameAuthHeader);
+            request.Headers.TryGetValue("Password", out PasswordAuthHeader);
+            if (UsernameAuthHeader.Count != 0 && PasswordAuthHeader.Count != 0)
+            {
+                string username = UsernameAuthHeader.First();
+                string password = PasswordAuthHeader.First();
+                return await userService.GetUser(username, password);
+            }
+            return null;
+        }
+    }
+}
+
---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.WEB/Controllers/UserController.cs
@@ -0,0 +1,214 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProjectManagementApp.BLL.Services;
+using ProjectManagementApp.DAL;
+using ProjectManagementApp.DAL.Models.Requests;
+using ProjectManagementApp.DAL.Entities;
+using ProjectManagementApp.WEB.Auth;
+using Common;
+using ProjectManagementApp.DAL.Models.Responses;
+using ProjectManagementApp.BLL.Validations;
+
+namespace ProjectManagementApp.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService userService;
+        private Validation validations;
+
+        public UserController(DatabaseContext database) : base()
+        {
+            userService = new UserService(database);
+            validations = new Validation(database);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            try
+            {
+                User currentUser = await userService.GetCurrentUser(Request);
+                validations.EnsureUserExist(currentUser);
+                validations.CheckRole(currentUser);
+
+                List<UserResponseModel> users = new List<UserResponseModel>();
+
+                foreach (User user in await userService.GetAllUsers())
+                {
+                    users.Add(new UserResponseModel()
+                    {
+                        Username = user.Username,
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        IsAdmin = user.IsAdmin,
+                        Id = user.Id
+                    });
+                }
+
+                return Ok(users);
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound(Constants.UserNotFound);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return Unauthorized(Constants.Unauthorized);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserResponseModel>> Get(int id)
+        {
+            try
+            {
+                User currentUser = await userService.GetCurrentUser(Request);
+                validations.EnsureUserExist(currentUser);
+                validations.CheckRole(currentUser);
+
+                User userFromDB = await userService.GetUser(id);
+
+                validations.EnsureUserExist(userFromDB);
+
+
+                return new UserResponseModel()
+                {
+                    Username = userFromDB.Username,
+                    FirstName = userFromDB.FirstName,
+                    LastName = userFromDB.LastName,
+                    IsAdmin = userFromDB.IsAdmin,
+                    Id = userFromDB.Id
+                };
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound(Constants.UserNotFound);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return Unauthorized(Constants.Unauthorized);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(UserRequestModel user)
+        {
+            try
+            {
+                User currentUser = await userService.GetCurrentUser(Request);
+
+                validations.EnsureUserExist(currentUser);
+                validations.CheckRole(currentUser);
+
+                bool isCreated = await userService.CreateUser(user.Username, user.Password, user.FirstName, user.LastName, user.IsAdmin, currentUser);
+
+                if (isCreated && ModelState.IsValid)
+                {
+                    User userFromDB = await userService.GetUser(user.Username);
+
+                    validations.EnsureUserExist(userFromDB);
+
+                    return CreatedAtAction(nameof(Post), new { id = userFromDB.Id }, Constants.CreatedUser);
+                }
+                else
+                {
+                    return BadRequest(Constants.FailedOperation);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound(Constants.UserNotFound);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return Unauthorized(Constants.Unauthorized);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserResponseModel>> Put(UserRequestModel user, int id)
+        {
+            try
+            {
+                User currentUser = await userService.GetCurrentUser(Request);
+                validations.EnsureUserExist(currentUser);
+                validations.CheckRole(currentUser);
+
+                User userFromDB = await userService.GetUser(id);
+
+                validations.EnsureUserExist(userFromDB);
+
+                if (await userService.EditUser(id, user.Username, user.Password, user.FirstName, user.LastName))
+                {
+                    User edited = await userService.GetUser(user.Username);
+
+                    return new UserResponseModel()
+                    {
+                        Username = edited.Username,
+                        FirstName = edited.FirstName,
+                        LastName = edited.LastName,
+                        IsAdmin = edited.IsAdmin,
+                        Id = edited.Id,
+                    };
+                }
+                else
+                {
+                    return BadRequest(Constants.FailedOperation);
+                }
+
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound(Constants.UserNotFound);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return Unauthorized(Constants.Unauthorized);
+            }
+            catch (BadImageFormatException)
+            {
+                return BadRequest(Constants.UserExist);
+            }
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                User currentUser = await userService.GetCurrentUser(Request);
+
+                validations.EnsureUserExist(currentUser);
+                validations.CheckRole(currentUser);
+
+                User userFromDB = await userService.GetUser(id);
+                validations.EnsureUserExist(userFromDB);
+
+                if (await userService.DeleteUser(userFromDB.Username))
+                {
+                    return Ok(Constants.DeletedUser);
+                }
+                else
+                {
+                    return BadRequest(Constants.FailedOperation);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound(Constants.UserNotFound);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return Unauthorized(Constants.Unauthorized);
+            }
+        }
+    }
+}
