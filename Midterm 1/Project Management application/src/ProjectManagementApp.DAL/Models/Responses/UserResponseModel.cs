b238fb7835e129331dc7b4e448281d8183397e72
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-18 22:12:57 +0200

    Created User models

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Models/Responses/UserResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementApp.DAL.Models.Responses
+{
+    public class UserResponseModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
