8fc306d29b5448ce01a77a043c27620f13b9aa74
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-18 22:14:04 +0200

    Implemented user validations

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.BLL/Validations/Validation.cs
@@ -0,0 +1,48 @@
+﻿using ProjectManagementApp.DAL;
+using ProjectManagementApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagementApp.BLL.Validations
+{
+    public class Validation
+    {
+        private readonly DatabaseContext database;
+
+        public Validation(DatabaseContext _database)
+        {
+            database = _database;
+        }
+
+        public bool EnsureUserExist(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool CheckRole(User user)
+        {
+            if (!user.IsAdmin)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool CheckUsername(string username)
+        {
+            return database.Users.Any(u => u.Username == username);
+        }
+    }
+}
