3411aaa00758337a0d9cbd7ea599c80e3934a6a6
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-19 13:28:56 +0200

    Created team models

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Models/Responses/TeamResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementApp.DAL.Models.Responses
+{
+    public class TeamResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
