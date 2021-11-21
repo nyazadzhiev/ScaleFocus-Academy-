f0a1ad940a6d1e938b3c5adbc57d62b7b3d4ede3
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-21 14:26:28 +0200

    Added new custom exceptions

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.BLL/Exceptions/TaskExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementApp.BLL.Exceptions
+{
+    public class TaskExistException : Exception
+    {
+        public TaskExistException() : base()
+        {
+
+        }
+    }
+}
