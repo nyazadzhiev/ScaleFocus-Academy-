af5efa9d8b45050deee6788aa98b15075d132623
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-21 14:26:45 +0200

    Added owner

--- Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Entities/ToDoTask.cs
+++ Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Entities/ToDoTask.cs
@@ -9,6 +9,8 @@ namespace ProjectManagementApp.DAL.Entities
         public string Description { get; set; }
         public int AsigneeId { get; set; }
         public virtual User Asignee { get; set; }
+        public int OwnerId { get; set; }
+        public virtual User Owner { get; set; }
         public bool IsCompleted { get; set; }
         public int ProjectId { get; set; }
         public virtual Project Project { get; set; }
