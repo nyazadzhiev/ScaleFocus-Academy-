04b766907894da5232f0eb514f44b7d22b50b0f9
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-22 20:32:16 +0200

    Fixed the response messages

--- Midterm 1/Project Management application/src/Common/Constants.cs
+++ Midterm 1/Project Management application/src/Common/Constants.cs
@@ -4,25 +4,17 @@ namespace Common
 {
     public static class Constants
     {
-        public const string UserNotFound = "User not found";
+        public const string NotFound = "{0} not found";
 
         public const string Unauthorized = "You don't have permission";
 
         public const string FailedOperation = "Something happened";
 
-        public const string CreatedUser = "User was created";
+        public const string Created = "{0} was created";
 
-        public const string DeletedUser = "User was deleted";
+        public const string Deleted = "{0} was deleted";
 
-        public const string UserExist = "User already exist";
-
-        public const string CreatedTeam = "Team was created";
-
-        public const string TeamNotFound = "Team not found";
-
-        public const string DeletedTeam = "Team was deleted";
-
-        public const string TeamExist = "Team already exist";
+        public const string Exist = "{0} already exist";
 
         public const string UserAddedToTeam = "User was added to team";
 
@@ -30,26 +22,8 @@ namespace Common
 
         public const string UserRemovedFromTeam = "User removed from team";
 
-        public const string CreatedProject = "Project was created";
-
-        public const string DeletedProject = "Project was deleted";
-
-        public const string ProjectExist = "Project already exist";
-
-        public const string ProjectNotFound = "Project not found";
-
         public const string TeamAddedToProject = "Team was added to project";
 
         public const string TeamRemovedFromProject = "Team was removed from project";
-
-        public const string TaskNotFound = "Task was not found";
-
-        public const string TaskExist = "Task already exist";
-
-        public const string DeletedTask = "Task was deleted";
-
-        public const string CreatedTask = "Task was created";
-
-        public const string CreatedWorkLog = "WorkLog was created";
     }
 }
