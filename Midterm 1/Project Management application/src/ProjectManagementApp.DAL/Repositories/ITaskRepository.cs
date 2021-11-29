55a4c298309946cf6ae9df7567be5c9d9cf6af42
nyazadzhiev <nyazadzhiev@gmail.com>
2021-11-29 14:18:49 +0200

    Created repository interfaces

---
+++ Midterm 1/Project Management application/src/ProjectManagementApp.DAL/Repositories/ITaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using ProjectManagementApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.DAL.Repositories
+{
+    public interface ITaskRepository
+    {
+        public Task AddTaskAsync(ToDoTask task);
+
+        public Task SaveChangesAsync();
+
+        public Task<ToDoTask> GetTaskAsync(int id);
+
+        public Task<ToDoTask> GetTaskAsync(string title);
+
+        public Task<List<ToDoTask>> GetTasksAsync(int projectId);
+
+        public void DeleteTask(ToDoTask task);
+    }
+}
