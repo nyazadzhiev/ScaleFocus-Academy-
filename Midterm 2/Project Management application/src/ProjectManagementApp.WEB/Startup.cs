6fafa5eef43446290547ad09b43ff9501b5d94b5
nyazadzhiev <nyazadzhiev@gmail.com>
2021-12-09 22:57:00 +0200

    Added identity to the app

--- Midterm 2/Project Management application/src/ProjectManagementApp.WEB/Startup.cs
+++ Midterm 2/Project Management application/src/ProjectManagementApp.WEB/Startup.cs
@@ -1,17 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProjectManagementApp.BLL;
+using ProjectManagementApp.BLL.Contracts;
 using ProjectManagementApp.BLL.Services;
 using ProjectManagementApp.BLL.Validations;
 using ProjectManagementApp.DAL;
+using ProjectManagementApp.DAL.Entities;
 using ProjectManagementApp.DAL.Repositories;
-using ProjectManagementApp.WEB.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,39 +34,101 @@ namespace ProjectManagementApp.WEB
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DatabaseContext>();
+            services.AddControllers();
 
-            var database = new DatabaseContext(Configuration);
-            DatabaseSeeder.Seed(database);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectManagementApp.WEB", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                            {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
+
+            services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<DatabaseContext>();
 
             services.AddTransient<IValidationService, ValidationService>();
 
-            services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<ITeamRepository, TeamRepository>();
             services.AddTransient<IProjectRepository, ProjectRepository>();
             services.AddTransient<ITaskRepository, TaskRepository>();
             services.AddTransient<IWorkLogRepository, WorkLogRepository>();
 
+            services.AddTransient<IUserManager, ProjectAppUserManager>();
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<ITeamService, TeamService>();
             services.AddTransient<IProjectService, ProjectService>();
             services.AddTransient<ITaskService, TaskService>();
             services.AddTransient<IWorkLogService, WorkLogService>();
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            var builder = services.AddIdentityServer((options) =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectManagementApp.WEB", Version = "v1" });
-            });
+                options.EmitStaticAudienceClaim = true;
+            })
+                       .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+                       .AddInMemoryClients(IdentityConfig.Clients);
 
+            builder.AddDeveloperSigningCredential();
+            builder.AddResourceOwnerValidator<PasswordValidator>();
+
+            services
+                .AddAuthorization()
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                    options.Audience = "https://localhost:5001/resources";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DatabaseContext database)
         {
+            DatabaseSeeder.Seed(app.ApplicationServices);
+
+            app.UseIdentityServer();
+
             if (env.IsDevelopment())
             {
                 database.Database.EnsureCreated();
@@ -77,6 +144,7 @@ namespace ProjectManagementApp.WEB
 
             app.UseMiddleware<ErrorHandlerMiddleware>();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
