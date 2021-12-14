8d82d15a66068991a5a7139c5fcc87a61b5195ac
nyazadzhiev <nyazadzhiev@gmail.com>
2021-12-14 19:21:40 +0200

    Added invalidInputException

---
+++ Midterm 2/Project Management application/src/ProjectManagementApp.BLL/Exceptions/InvalidInputException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementApp.BLL.Exceptions
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message)
+        {
+
+        }
+    }
+}
